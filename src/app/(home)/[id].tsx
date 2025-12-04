d3b606ff4d3c4841c8d60b8019ae1c509c396087
Akram Imtiaz <akram.imtiaz10@gmail.com>
2025-12-04 21:15:15 +0800

    building out the anime details page

--- src/app/(home)/[id].tsx
+++ src/app/(home)/[id].tsx
@@ -1,8 +1,37 @@
+import { fetchAnimeById } from '@/src/api/jikan';
+import { FetchAnimeByIdResponse as Anime } from '@/src/types/jikan';
+import { Image } from 'expo-image';
+import { useLocalSearchParams } from 'expo-router';
+import { useEffect, useState } from 'react';
 import { Text, View } from 'react-native';
 
 export default function Details() {
+    const { id } = useLocalSearchParams();
+    const [anime, setAnime] = useState<Anime | null>(null);
+
+    useEffect(() => {
+        const load = async () => {
+            const _anime = await fetchAnimeById(Number(id));
+            setAnime(_anime);
+        };
+
+        load();
+    }, [id]);
+
+    if (!anime) {
+        return null;
+    }
+
     return (
         <View>
+            <Image
+              style={{
+                height: 600,
+                width: "auto",
+              }}
+              source={anime.images.webp.large_image_url}
+              contentFit="cover"
+            />
            <Text>Id Screen</Text> 
         </View>
     )
