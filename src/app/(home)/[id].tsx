4687c680c9762364c89bd6d0a5c24b63246cd1fe
Akram Imtiaz <akram.imtiaz10@gmail.com>
2025-12-05 04:27:40 +0800

    risky optimisation

--- src/app/(home)/[id].tsx
+++ src/app/(home)/[id].tsx
@@ -1,38 +1,30 @@
-import { fetchAnimeById } from '@/src/api/jikan';
-import { FetchAnimeByIdResponse as Anime } from '@/src/types/jikan';
-import { Image } from 'expo-image';
-import { useLocalSearchParams } from 'expo-router';
-import { useEffect, useState } from 'react';
-import { Text, View } from 'react-native';
+import { Image } from "expo-image";
+import { Stack, useLocalSearchParams, useRouter } from 'expo-router';
+import { Text, View } from "react-native";
 
 export default function Details() {
-    const { id } = useLocalSearchParams();
-    const [anime, setAnime] = useState<Anime | null>(null);
+  const router = useRouter();
+  const { animeJson } = useLocalSearchParams();
 
-    useEffect(() => {
-        const load = async () => {
-            const _anime = await fetchAnimeById(Number(id));
-            setAnime(_anime);
-        };
+  const anime = JSON.parse(animeJson as string);
 
-        load();
-    }, [id]);
+  if (!anime) {
+    router.dismiss();
+    return;
+  }
 
-    if (!anime) {
-        return null;
-    }
-
-    return (
-        <View>
-            <Image
-              style={{
-                height: 600,
-                width: "auto",
-              }}
-              source={anime.images.webp.large_image_url}
-              contentFit="cover"
-            />
-           <Text>Id Screen</Text> 
-        </View>
-    )
-};
+  return (
+    <View>
+      <Stack.Screen options={{ title: anime.title }} />
+      <Image
+        style={{
+          height: 600,
+          width: "auto",
+        }}
+        source={anime.images.webp.large_image_url}
+        contentFit="cover"
+      />
+      <Text>Id Screen</Text>
+    </View>
+  );
+}
--- src/components/AnimeListCard.tsx
+++ src/components/AnimeListCard.tsx
@@ -1,39 +1,60 @@
-import { Image } from 'expo-image';
-import { Link } from 'expo-router';
-import { Pressable } from 'react-native';
-import styled from 'styled-components/native';
-import { FetchAnimeByIdResponse as Anime } from '../types/jikan';
-import Typography from './shared/Typography';
+import { Image } from "expo-image";
+import { useRouter } from "expo-router";
+import { useCallback } from "react";
+import { Pressable } from "react-native";
+import styled from "styled-components/native";
+import { FetchAnimeByIdResponse as Anime } from "../types/jikan";
+import Typography from "./shared/Typography";
 
 interface AnimeListCardProps {
-    anime: Anime;
-};
+  anime: Anime;
+}
 
 export default function AnimeListCard({ anime }: AnimeListCardProps) {
-    return (
-      <ItemContainer>
-        <Link href={`/${anime.mal_id}`} push asChild>
-          <Pressable>
-            <Image
-              cachePolicy="memory-disk"
-              recyclingKey={String(anime.mal_id)}
-              style={{
-                height: 300,
-                width: "auto",
-              }}
-              source={anime.images.webp.large_image_url}
-              contentFit="cover"
-            />
-            <Typography numberOfLines={1}>{anime.title}</Typography>
-          </Pressable>
-        </Link>
-        <Typography>{anime.score}</Typography>
-      </ItemContainer>
-    );
+  const router = useRouter();
+
+  const goToDetail = useCallback(() => {
+    const toStringify: Anime = {
+      mal_id: anime.mal_id,
+      title: anime.title,
+      rating: anime.rating,
+      score: anime.score,
+      scored_by: anime.scored_by,
+      synopsis: anime.synopsis,
+      images: anime.images,
+    };
+
+    router.push({
+      pathname: "/[id]",
+      params: {
+        id: anime.mal_id,
+        animeJson: JSON.stringify(toStringify),
+      },
+    });
+  }, [anime, router]);
+
+  return (
+    <ItemContainer>
+      <Pressable onPress={goToDetail}>
+        <Image
+          cachePolicy="memory-disk"
+          recyclingKey={String(anime.mal_id)}
+          style={{
+            height: 300,
+            width: "auto",
+          }}
+          source={anime.images.webp.large_image_url}
+          contentFit="cover"
+        />
+        <Typography numberOfLines={1}>{anime.title}</Typography>
+      </Pressable>
+      <Typography>{anime.score}</Typography>
+    </ItemContainer>
+  );
 }
 
 const ItemContainer = styled.View`
-    flex-direction: column;
-    gap: 6px;
-    margin: 8px;
+  flex-direction: column;
+  gap: 6px;
+  margin: 8px;
 `;
