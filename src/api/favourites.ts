6a306aaa3b8857e2e7ae28ee0f1091e2d9ee0578
Akram Imtiaz <akram.imtiaz10@gmail.com>
2025-12-08 09:08:55 +0800

    refactor api/favourites

--- src/api/favourites.ts
+++ src/api/favourites.ts
@@ -18,17 +18,27 @@ export async function getFavouritesFromStorage(): Promise<
 }
 
 export async function addFavourite(anime: Anime) {
-  const favourites = await getFavouritesFromStorage();
-  const updated = { ...favourites, [anime.mal_id]: anime };
+  try {
+    const favourites = await getFavouritesFromStorage();
+    const updated = { ...favourites, [anime.mal_id]: anime };
 
-  await AsyncStorage.setItem(STORAGE_KEY, JSON.stringify(updated));
-  return updated;
+    await AsyncStorage.setItem(STORAGE_KEY, JSON.stringify(updated));
+    return updated;
+  } catch (err) {
+    console.error(`[${STORAGE_KEY}] SAVE FAVOURITE TO STORAGE:`, err);
+    throw err;
+  }
 }
 
 export async function removeFavourite(anime: Anime) {
-  const favourites = await getFavouritesFromStorage();
-  const { [anime.mal_id]: _, ...updated } = favourites;
+  try {
+    const favourites = await getFavouritesFromStorage();
+    const { [anime.mal_id]: _, ...updated } = favourites;
 
-  await AsyncStorage.setItem(STORAGE_KEY, JSON.stringify(updated));
-  return updated;
+    await AsyncStorage.setItem(STORAGE_KEY, JSON.stringify(updated));
+    return updated;
+  } catch (err) {
+    console.error(`[${STORAGE_KEY}] REMOVE FAVOURITE FROM STORAGE:`, err);
+    throw err;
+  }
 }
