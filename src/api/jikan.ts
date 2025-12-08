28a29c09b01386325cb44065c0f11f91ac908ee0
Akram Imtiaz <akram.imtiaz10@gmail.com>
2025-12-08 09:46:11 +0800

    refactoring within api/jikan

--- src/api/jikan.ts
+++ src/api/jikan.ts
@@ -7,28 +7,26 @@ import { buildUrl } from "../utils";
 
 const BASE_API_URL = "https://api.jikan.moe/v4";
 
-export async function fetchAnimeList(
-  page: number = 1,
+export async function fetchAnimesByGenre(
   genreId?: number,
+  page: number = 1,
 ): Promise<FetchAnimeListResponse> {
+  const queryParams = {
+    page,
+    limit: 20,
+    order_by: "popularity",
+    sfw: true,
+    ...(genreId && { genres: genreId }),
+  };  
+  const url = buildUrl(BASE_API_URL, "/anime", queryParams);
+  
   try {
-    const baseQueryParams = {
-      page,
-      limit: 20,
-      order_by: "popularity",
-      sfw: true,
-    };
-    const queryParams = genreId
-      ? { ...baseQueryParams, genres: genreId }
-      : baseQueryParams;
-    const url = buildUrl(BASE_API_URL, "/anime", queryParams);
-
     const response = await fetch(url);
     const data = await response.json();
 
     return data;
   } catch (err) {
-    console.error(err);
+    console.error(`GET ${url}:`, err);
     throw err;
   }
 }
@@ -36,25 +34,29 @@ export async function fetchAnimeList(
 export async function fetchAnimeById(
   id: number,
 ): Promise<FetchAnimeByIdResponse> {
+  const url = buildUrl(BASE_API_URL, `/anime/${id}`);
+
   try {
-    const response = await fetch(buildUrl(BASE_API_URL, `/anime/${id}`));
+    const response = await fetch(url);
     const data = await response.json();
 
     return data.data;
   } catch (err) {
-    console.error(err);
+    console.error(`GET ${url}:`, err);
     throw err;
   }
 }
 
 export async function fetchAnimeGenres(): Promise<FetchAnimeGenresResponse[]> {
+  const url = buildUrl(BASE_API_URL, `/genres/anime`);
+
   try {
-    const response = await fetch(buildUrl(BASE_API_URL, `/genres/anime`));
+    const response = await fetch(url);
     const data = await response.json();
 
     return data.data;
   } catch (err) {
-    console.error(err);
+    console.error(`GET ${url}:`, err);
     throw err;
   }
 }
