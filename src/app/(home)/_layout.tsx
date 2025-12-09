d94bd6c8eba6e95f3937b4116fdc762504c4a15b
Akram Imtiaz <akram.imtiaz10@gmail.com>
2025-12-09 08:35:54 +0800

    add status bar to home layout

--- src/app/(home)/_layout.tsx
+++ src/app/(home)/_layout.tsx
@@ -1,9 +1,11 @@
 import { Stack } from "expo-router";
+import { StatusBar } from "expo-status-bar";
 import { Fragment } from "react";
 
 export default function Layout() {
   return (
     <Fragment>
+      <StatusBar style="auto" />
       <Stack>
         <Stack.Screen name="index" options={{ title: "Home" }} />
         <Stack.Screen name="[id]" options={{ title: "Details" }} />
